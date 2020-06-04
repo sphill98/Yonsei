9e5dd732b9f43139b95a6c0c0fdb642ce8a8fac6
sphill98 <45838750+sphill98@users.noreply.github.com>
2020-06-04 20:34:31 +0900

    Create score.js

---
+++ score.js
@@ -0,0 +1,8 @@
+// 4점, '딱히 없습니다' 일괄 평가
+// 강의평가할 과목 선택 후 콘솔(F12) 열어서 복붙엔터
+// 수정하고 싶은 값 있는 경우 confirm 취소하고 수정 후 맨 뒤로 가서 완료 버튼 선택
+
+$("input[value='4']").click();
+$("textarea").val("딱히 없습니다.");
+$("td.al.w2tb_noTH > input").click();
+$("span#btnSave_span").click();
